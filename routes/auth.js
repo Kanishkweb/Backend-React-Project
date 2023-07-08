3c1c8b794f21759b018a58b5681eecfd8fed76e8
kanishk <kanishkshakya1@gmail.com>
2023-07-08 11:22:33 +0530

    Added Validation to Schema model Name,Email,Pass

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const User = require('../models/User');
-
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.post('/', (req,res)=> {  // Create a user using the POST "/api/auth". Doesn't requrie auth
-    // res.send('Hello Kanishk')
-    const user = User(req.body);
-    user.save()
-    console.log(req.body)
-    res.send(req.body)
-})
+router.post('/createuser', [
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  body('name').notEmpty().withMessage('Name is required')
+], async (req, res) => {
+  // If errors return bad request and Errors.
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-module.exports = router
+  try {
+    const { email, password, name } = req.body;    // Destructuring 
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    user = new User({
+      email,
+      password,
+      name
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
